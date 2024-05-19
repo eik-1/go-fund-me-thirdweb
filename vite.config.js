3c8abfed6b467cc7730fa39ebf02b6ee40a5b91f
Sarthak <sarthakrawatbiz@gmail.com>
2024-05-19 18:57:47 +0530

    Removed pollyfills

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
-import polyfillNode from "rollup-plugin-polyfill-node";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), polyfillNode()],
-  resolve: {
-    alias: {
-      crypto: "crypto-browserify",
-      stream: "stream-browserify",
-    },
-  },
-  define: {
-    global: "globalThis", // Define global as globalThis
-  },
+  plugins: [react(), eslint()],
 });
